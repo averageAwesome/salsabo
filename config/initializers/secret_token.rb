65b8c7ca022686b308000dac15a0f0d5ccdbb58d
dennistetsenko <denystetsenko@gmail.com>
2013-09-09 12:38:58 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Salsabo::Application.config.secret_key_base = 'd65c77ae0a85cf331cab1e8540199683906dabba35e1a0b5b990646d0859ced8a265974c60132a91d694a57a804c13e5fe7c316b0b2fd39768c8fab70f674e3b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Salsabo::Application.config.session_store :cookie_store, key: '_salsabo_session'
